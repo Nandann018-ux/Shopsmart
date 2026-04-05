ab4301f266ae1082a78d50172836510fda792bf2
Nandan Acharya <samarthachar18@gmail.com>
2026-04-05 23:51:27 +0530

    feat(routing): add /shop route to application

--- urbangear-frontend/src/App.jsx
+++ urbangear-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Shop from './pages/Shop';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
         {/* Additional routes can be added here */}
       </Routes>
     </Router>
